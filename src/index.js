2231e443ea905d9f2de4d0aafb40acd056301bb2
ana4is <anaisflechier@gmail.com>
2022-04-23 18:31:44 +0200

    JS current date + time

---
+++ src/index.js
@@ -0,0 +1,39 @@
+function currentDate() {
+  let now = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let date = now.getDate();
+  let month = months[now.getMonth()];
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let currentDate = document.querySelector("#current-date");
+  currentDate.innerHTML = `${day}, ${month} ${date}`;
+  let currentTime = document.querySelector("#current-time");
+  currentTime.innerHTML = `${hours}:${minutes}`;
+}
+currentDate();
